592ebed30bf28f6e3ce576a733c68b4982c65ba6
Timothy Henry <timhenry@mit.edu>
2018-05-04 12:17:37 -0400

    Fetch live tweets matching given keywords

--- data/twitter/data_collection.py
+++ data/twitter/data_collection.py
@@ -1,19 +1,54 @@
-# system
-import re
 import time
-import xlsxwriter
 import tweepy
 from tweepy import OAuthHandler
+from tweepy.models import Status
 import json
 import pickle
+from multiprocessing import Process
+from time import sleep
 
+class StreamListener(tweepy.StreamListener):
 
-def is_number(s):
-    try:
-        float(s)
-        return True
-    except ValueError:
-        return False
+    def __init__(self, client):
+        self.client = client
+        tweepy.StreamListener.__init__(self, client.api)
+
+    def on_status(self, status):
+        """
+        Handles statuses matching filter
+        """
+        # Additional filter for tweets lacking geographic info
+        if not status.place:
+            return
+
+        # Get full tweet text
+        try:
+            text = status.extended_tweet["full_text"]
+        except AttributeError:
+            text = status.text
+
+        tweet = status
+        if tweet.retweeted or text.startswith("RT "):
+            return
+
+        coords = json.dumps(tweet.place.bounding_box.coordinates[0])
+        data = [{
+            'content': text,
+            'created_at': tweet.created_at.strftime(self.client.TIME_FORMAT),
+            'favorites': tweet.favorite_count,
+            'retweets': tweet.retweet_count,
+            'lang': tweet.lang,
+            'coordinates': coords
+        }]
+
+        # Save tweet
+        with open('./data/twitter/stream/' + str(tweet.id) + '.json', 'w') as f:
+            json.dump(data, f)
+
+    def on_error(self, status_code):
+        if status_code == 420:
+            #returning False in on_data disconnects the stream
+            return False
 
 
 class TwitterClient:
@@ -23,7 +58,6 @@ class TwitterClient:
     """
 
     class TwitterLists:
-
         def __init__(self, client):
             self.api = client.api
 
@@ -47,6 +81,7 @@ class TwitterClient:
     __TIME_FORMAT = '%Y-%m-%d %H:%M:%S'
 
     def __init__(self):
+        self.TIME_FORMAT = '%Y-%m-%d %H:%M:%S'
 
         with open("./data/twitter/credentials.txt") as f:
             lines = f.read().splitlines()
@@ -56,6 +91,9 @@ class TwitterClient:
         self.auth.set_access_token(access_token, access_token_secret)
         self.api = tweepy.API(self.auth)
         self.twitter_lists = TwitterClient.TwitterLists(self)
+        self.listener = StreamListener(self)
+        self.stream = tweepy.Stream(auth=self.auth, listener=self.listener, tweet_mode='extended')
+
 
     def get_user_tweets(self,
                         screen_name,
@@ -104,6 +142,33 @@ class TwitterClient:
 
         return data
 
+    def stream_to_timeout(self, keywords, timeout=10):
+
+        """ Streams with a time limit
+
+        :param keywords: the keywords to filter by
+        :param timeout: The time limit in seconds
+        :return: True if the function ended successfully. False if it was terminated.
+        """
+
+        p = Process(target=self.stream.filter,kwargs={'track':keywords})
+        p.start()
+        p.join(timeout)
+        if p.is_alive():
+            p.terminate()
+            return False
+
+        return True
+
+
+def stream(keywords, iters = 20, timeout = 10):
+
+    client = TwitterClient()
+
+    for _ in range(iters):
+        client.stream_to_timeout(keywords, timeout)
+        sleep(2)
+
 
 def main():
 
@@ -146,4 +211,6 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    keywords = ['trump']
+    stream(keywords, iters=20, timeout=10)
+    #main()
